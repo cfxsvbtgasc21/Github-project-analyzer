c3e0b75048de0b560d2b9563ccaf87b2a70836f7
JiaMaster <zl15980162443@163.com>
2024-12-29 14:25:26 +0800

    Add files via upload
    添加了帮助信息

--- home_window.py
+++ home_window.py
@@ -14,6 +14,7 @@ class MainApp(QWidget):
         self.ui_home.spider_issues.clicked.connect(self.show_spider_issues_window)
         self.ui_home.spider_commits.clicked.connect(self.show_spider_commits_window)
         self.ui_home.exit.clicked.connect(self.quit)
+        self.ui_home.help.clicked.connect(self.show_helps)
     def show_spider_issues_window(self):
         from Spider_issue_window import Spider_issue_window
         self.Spider_window=Spider_issue_window()
@@ -24,6 +25,11 @@ class MainApp(QWidget):
         self.Spider_window = Spider_commit_window()
         self.Spider_window.show()
         self.close()
+    def show_helps(self):
+        self.message_help = QMessageBox()
+        self.message_help.setWindowTitle("帮助")
+        self.message_help.setText("这是个Github项目的分析器，里面包括对象的issues分析和commmits分析。issues分析可以得到主要问题的信息和解决时间对比。\ncommmits分析是对该项目提交记录基于作者和日期的分析。")
+        self.message_help.show()
     def quit(self):
         exit(0)
 if __name__ == "__main__":
